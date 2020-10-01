b9dbaf1b8d9b9d384755c1977ac387f0d9212262
rami-gamal <rami.gamal.mahmoud@gmail.com>
2020-10-01 22:19:52 +0200

    Removing empty lines

--- Matjar/DashBoard.cs
+++ Matjar/DashBoard.cs
@@ -6,20 +6,18 @@ namespace Matjar
 {
     public partial class DashBoard : Form
     {
-
         public DashBoard()
         {
             InitializeComponent();
-
         }
 
         private void DashBoard_Load(object sender, EventArgs e)
         {
-            btn_daly_sales.PerformClick();
-
+            //btn_daly_sales.PerformClick();
+            btn_purchases.PerformClick();
         }
 
-        private void setButtonsBackColor(object sender)
+        private void SetActiveButton(object sender)
         {
             Button current = (Button)sender;
             pnl_pointer.Location = new Point(pnl_pointer.Location.X, current.Location.Y);
@@ -37,40 +35,34 @@ namespace Matjar
 
         private void btn_goods_Click(object sender, EventArgs e)
         {
-            setButtonsBackColor(sender);
+            SetActiveButton(sender);
             storeUC1.BringToFront();
             storeUC1.Start();
         }
         private void btn_daly_sales_Click(object sender, EventArgs e)
         {
-            setButtonsBackColor(sender);
+            SetActiveButton(sender);
             uC_Sales1.BringToFront();
             uC_Sales1.Start();
         }
 
         private void btn_purchases_Click(object sender, EventArgs e)
         {
-            setButtonsBackColor(sender);
+            SetActiveButton(sender);
             uC_Purchases1.BringToFront();
             uC_Purchases1.Start();
         }
 
         private void btn_statistics_Click(object sender, EventArgs e)
         {
+            SetActiveButton(sender);
             new FormManagement().ShowDialog();
         }
 
         private void btn_new_product_Click(object sender, EventArgs e)
         {
-            setButtonsBackColor(sender);
+            SetActiveButton(sender);
             new AddUpdateProductForm().Show();
         }
-
-
-
-
-
     }
-
-
 }
--- Matjar/Program.cs
+++ Matjar/Program.cs
@@ -1,21 +1,20 @@
 ﻿using System;
 using System.Windows.Forms;
-using System.Collections.Generic;
 
 namespace Matjar
 {
-    static class Program
+  static class Program
+  {
+
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
     {
-        static List<string> model = new List<string>();
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new DashBoard());
-        }
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new DashBoard());
     }
+  }
 }
