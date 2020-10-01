fba5f29ee800582ac400e3801c4fc0ea82844ca6
rami-gamal <rami.gamal.mahmoud@gmail.com>
2020-10-01 22:50:27 +0200

    Make the class disposable

--- Repos/ProductsRepo.cs
+++ Repos/ProductsRepo.cs
@@ -1,6 +1,18 @@
-﻿namespace Repos
+﻿using DataAccessLayer;
+using System;
+
+namespace Repos
 {
-    public class ProductsRepo
+    public class ProductsRepo : IDisposable
     {
+        private Connection conn;
+        public ProductsRepo()
+        {
+            this.conn = new Connection();
+        }
+        public void Dispose()
+        {
+            this.conn.Dispose();
+        }
     }
 }
