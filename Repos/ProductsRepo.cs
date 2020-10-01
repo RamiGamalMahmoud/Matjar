bc4c6e515571833a33e81b3253eda79123f17351
rami-gamal <rami.gamal.mahmoud@gmail.com>
2020-10-02 00:18:21 +0200

    Add GetAll and UpdateProduct methodes

--- Repos/ProductsRepo.cs
+++ Repos/ProductsRepo.cs
@@ -1,5 +1,7 @@
 ﻿using DataAccessLayer;
 using System;
+using QueryBuilder;
+using System.Data;
 
 namespace Repos
 {
@@ -13,6 +15,32 @@ namespace Repos
         public void Dispose()
         {
             this.conn.Dispose();
+        }
+
+        public DataTable UpdateProduct(string[] data, string productId)
+        {
+            using (Query query = new Query())
+            {
+                string[] columns = { "selling_price", "purchasing_price", "profit_margin" };
+                query.Update("products")
+                    .SetValues(columns, data)
+                    .Where("product_id", "=", productId);
+                int result = this.conn.Run(query.QueryString, query.QueryParams);
+                Console.WriteLine(query.QueryString);
+                Console.WriteLine(result);
+            }
+
+            return null;
+        }
+
+        public DataTable GetAllProducts()
+        {
+            using (Query query = new Query())
+            {
+                query.Select("*")
+                    .From("products_data_view");
+                return this.conn.GetAll(query.QueryString);
+            }
         }
     }
 }
