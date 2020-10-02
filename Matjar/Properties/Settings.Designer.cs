bbb2d11fe05637711d616142a07b4c0ea47fb105
rami-gamal <rami.gamal.mahmoud@gmail.com>
2020-10-03 01:41:10 +0200

    Add items for application settings

--- Matjar/Properties/Settings.Designer.cs
+++ Matjar/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Matjar.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,6 +20,24 @@ namespace Matjar.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("54, 79, 107")]
+        public global::System.Drawing.Color BaseColor {
+            get {
+                return ((global::System.Drawing.Color)(this["BaseColor"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("244, 250, 156")]
+        public global::System.Drawing.Color ActiveButtonBackColor {
+            get {
+                return ((global::System.Drawing.Color)(this["ActiveButtonBackColor"]));
             }
         }
     }
