b38be5d49275f3625aa9392a1d340f625423940c
rami-gamal <rami.gamal.mahmoud@gmail.com>
2020-10-03 16:34:20 +0200

    Initial commit

---
+++ Repos/CategoriesRepo.cs
@@ -0,0 +1,32 @@
+﻿using DataAccessLayer;
+using System;
+using QueryBuilder;
+using System.Data;
+
+namespace Repos
+{
+    public class CategoriesRepo : IDisposable
+    {
+        private Connection conn;
+
+        public CategoriesRepo()
+        {
+            this.conn = new Connection();
+        }
+
+        public void Dispose()
+        {
+            this.conn.Dispose();
+        }
+
+        public DataTable GetCategories()
+        {
+            using (Query query = new Query())
+            {
+                query.Select("id, category ")
+                  .From("categories");
+                return this.conn.GetAll(query.QueryString);
+            }
+        }
+    }
+}
